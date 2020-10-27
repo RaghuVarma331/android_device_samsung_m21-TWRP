0cd43b4bb9d37ecff81c8a5913550fe3a8fe0442
RaghuVarma331 <raghuvarma331@gmail.com>
2020-10-27 09:20:31 +0000

    M21: add PRODUCT_MODEL
    Signed-off-by: RaghuVarma331 <raghuvarma331@gmail.com>

--- omni_m21.mk
+++ omni_m21.mk
@@ -23,5 +23,6 @@ $(call inherit-product, device/samsung/m21/device.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_m21
 PRODUCT_DEVICE := m21
+PRODUCT_MODEL := SM-M215F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
