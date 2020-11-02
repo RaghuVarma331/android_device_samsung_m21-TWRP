3e6e566cba4381c5e91c2ab39d29c1939eee3c4b
Ian Macdonald <ian@caliban.org>
2020-11-02 14:39:56 +0100

    M21: use system.prop instead of device.mk for PRODUCT_USE_DYNAMIC_PARTITIONS
    'PRODUCT_USE_DYNAMIC_PARTITIONS := true' causes the following warning to
    be repeatedly logged:
    
    11-02 08:40:10.912  4727  4727 W ServiceManagement: Waited for hwservicemanager.ready for a second, waiting another...

--- omni_m21.mk
+++ omni_m21.mk
@@ -17,9 +17,6 @@
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Include any options that can't be included in BoardConfig.mk
-$(call inherit-product, device/samsung/m21/device.mk)
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_m21
 PRODUCT_DEVICE := m21
